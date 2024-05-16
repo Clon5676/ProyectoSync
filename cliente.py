38e213473896bd0ed76f6f570e19444e4ea8ea69
fatimaavila <fatimaavila@ufm.edu>
2024-05-16 17:14:29 -0600

    cliente nuevo

--- cliente.py
+++ cliente.py
@@ -1,3 +1,42 @@
+import socket
+import sys
+import time
+
+class Client:
+    def __init__(self, server_ip, server_port, path_to_scan):
+        self.server_ip = server_ip
+        self.server_port = server_port
+        self.path_to_scan = path_to_scan
+
+    def start(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+            print(f'Connecting to {self.server_ip}:{self.server_port}')
+            client_socket.connect((self.server_ip, self.server_port))
+            print(client_socket.recv(1024).decode())
+            while True:
+                print(f'Sending path to scan: {self.path_to_scan}')
+                client_socket.sendall(self.path_to_scan.encode())
+                response = client_socket.recv(1024).decode()
+                print(response)
+                if 'Request accepted' in response:
+                    break
+                time.sleep(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Usage: client.py <server_ip> <server_port> <path_to_scan>')
+        sys.exit(1)
+    server_ip = sys.argv[1]
+    server_port = int(sys.argv[2])
+    path_to_scan = sys.argv[3]
+
+    client = Client(server_ip, server_port, path_to_scan)
+    client.start()
+
+
+
+
+
 
 # import socket
 # import argparse
@@ -43,7 +82,7 @@
 # # Cerrar la conexión con el servidor
 # client_socket.close()
 
-import socket
+""" import socket
 import argparse
 import time
 
@@ -75,4 +114,4 @@ folder_content = client_socket.recv(1024).decode()
 print("Folder content:", folder_content)
 
 # Cerrar la conexión con el servidor
-client_socket.close()
+client_socket.close() """
